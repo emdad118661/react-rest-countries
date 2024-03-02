5ab790f1208b21ed399c483f151af9111e3ac604
emdad118661 <emdad118661@gmail.com>
2024-03-02 23:31:49 +0600

    conditional css

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -10,13 +10,16 @@ const Country = (props) => {
           setVisited(!visited)
     }
     return (
-        <div className='country'>
-            <h2>Country Name: {props.name}</h2>
+        <div className={`country ${visited && 'visited'}`}>
+            <h2 style={{color: visited ? 'purple':'black'}}>Country Name: {props.name}</h2>
             <img src={flags.png} alt="" />
             <p>Population: {props.country.population}</p>
             <p><small>Region: {region}</small></p>
             <p><small>Area: {area}</small></p>
             <p><small>Code: {cca3}</small></p>
+            <button>Mark Visited</button>
+            <br/>
+            <br/>
             <button onClick={handleVisited}>{visited ? "visited" : "Will go"}</button>
             {
                 visited ? ' I have visited':' I want to visit'
