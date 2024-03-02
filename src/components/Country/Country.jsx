3555631a48b6cd6d9470758e89188b5b67d55144
emdad118661 <emdad118661@gmail.com>
2024-03-02 20:06:46 +0600

    visited event handle

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './Country.css';
 
 const Country = (props) => {
-    const {area, region, flags} = props.country;
+    const {area, region, flags, cca3} = props.country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+          setVisited(!visited)
+    }
     return (
         <div className='country'>
             <h2>Country Name: {props.name}</h2>
@@ -9,6 +16,11 @@ const Country = (props) => {
             <p>Population: {props.country.population}</p>
             <p><small>Region: {region}</small></p>
             <p><small>Area: {area}</small></p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? "visited" : "Will go"}</button>
+            {
+                visited ? ' I have visited':' I want to visit'
+            }
         </div>
     );
 };
